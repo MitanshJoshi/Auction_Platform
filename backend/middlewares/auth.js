eff8a97873b93719efe42310128bc9ac8ee90e5f
MitanshJoshi <mitansh.joshi141203@gmail.com>
2024-11-21 15:18:39 +0530

    bug fixed

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -4,11 +4,11 @@ import ErrorHandler from "./error.js";
 
 export const isAuthenticated = async (req, res, next) => {
     const token = req.cookies.token;
-
+    console.log(token);
+    
     if(!token)
     {
         return next(new ErrorHandler("user not authenticated",400));
-
     }
     const decoded = jwt.verify(token,process.env.JWT_SECRET);
     req.user = await User.findById(decoded.id);
